5f5e35b6bf8977c7facee7d447700532e3a61eba
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-05-21 22:23:00 +0200

    done

--- sign in/main.js
+++ sign in/main.js
@@ -1,3 +1,6 @@
+document.getElementsByClassName("boja")[0].style.color = "rgb(189, 47, 70)";
+document.getElementsByClassName("boja")[0].style.padding = "9px";
+
 function validateUsername() {
   const usernameInput = document.getElementById("username");
   const usernameError = document.getElementById("usernameError");
