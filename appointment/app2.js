1d11f5b8db65e405de14b23818be13d8d2faa378
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-05-23 12:48:37 +0200

    javascript

--- appointment/app2.js
+++
@@ -1,9 +0,0 @@
-var currentLocation = window.location.href;
-var navLinks = document.querySelectorAll("header ul li a");
-
-for (var i = 0; i < navLinks.length; i++) {
-  var link = navLinks[i];
-  if (link.href === currentLocation) {
-    link.classList.add("active");
-  }
-}
